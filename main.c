48a759cf3c3a2c47786bad5ea5253630055daa99
benjibyte <benjipixel@proton.me>
2026-08-28 17:27:26 -0500

    Compiling!

--- main.c
+++ main.c
@@ -5,8 +5,23 @@
 // Okay! Here goes!
 int main(void) {
   // Window Initialization
-  
+
+  const int screenW = 320;
+  const int screenH = 180;
+  InitWindow(screenW, screenH, "My First real Raylib Window!");
+  SetTargetFPS(60);
+
+  // Game Loop
+  while (!WindowShouldClose) {
+    // Logic Updates go here ...
+
+    // Drawing the Window and Graphics
+    BeginDrawing();
+      ClearBackground(RAYWHITE);
+      DrawText("Congrats! I created my first window!", 190, 200, 20, LIGHTGRAY);
+    EndDrawing();
+  } // End of Game loop
 
 
-  return 0;
+  return 0; // Close program
 }
