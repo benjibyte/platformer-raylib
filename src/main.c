554b577e702d0c89f1a979a8e0c5074e354a6b7d
benjibyte <benjipixel@proton.me>
2026-08-28 19:57:07 -0500

    Added a background

--- src/main.c
+++ src/main.c
@@ -21,8 +21,15 @@ int main(void) {
 
   // Load Assets into memory, and sharpen Pixel art
   Image PlayerBox = LoadImage("assets/collisionbox.png");
-  ImageResize(&PlayerBox, 32, 64); // scale 2x
+    // Get the image into a raylib texture and set the rendering 
+    // filter to be "point" or in otherwords nearest-neighbor
   Texture2D player = LoadTextureFromImage(PlayerBox);
+  SetTextureFilter(player, TEXTURE_FILTER_POINT);
+
+  // Create the virtual render canvas 
+  RenderTexture2D canvas = LoadRenderTexture(virtualW, virtualH);
+  SetTextureFilter(canvas.texture, TEXTURE_FILTER_POINT);
+  // Get rid of the no longer needed image data to save space in memory
   UnloadImage(PlayerBox);
 
   Vector2 playerPosition = { (float)screenW/2, (float)screenH/2 };
@@ -37,12 +44,19 @@ int main(void) {
     
 
 
-    // Drawing the Window and Graphics
+    // Drawing virtual pixel art Graphics
+
+    BeginTextureMode(canvas);
+      ClearBackground(BLACK);
+      
+      // Because we are drawing the player on an actually small virtual 
+      // canvas, we got to convert the calculated position back to integers.
+      DrawTexture(player, (int)playerPosition.x, (int)playerPosition.y, WHITE); 
+    EndTextureMode();
+
+    // Draw the virtual graphics o the window
     BeginDrawing();
-      ClearBackground(RAYWHITE);
-      
-      DrawTexture(player, playerPosition.x, playerPosition.y, WHITE);
-      
+      ClearBackground(DARKGRAY);
 
     EndDrawing();
   } // End of Game loop
