f80adb74526e0c6d9740521098fe1f8f0adb784f
benjibyte <benjipixel@proton.me>
2026-08-28 19:30:55 -0500

    Preparing the game window for rendering pixel art...

--- src/main.c
+++ src/main.c
@@ -2,16 +2,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+const float playerVelocity = 8.0f;
+
 // Okay! Here goes!
 int main(void) {
   // Window Initialization
-  const int screenW = 1280;
-  const int screenH = 720;
+  
+  const int virtualW = 320;
+  const int virtualH = 180;
+
+  const int scale = 4;
+  const int screenW = virtualW * scale;
+  const int screenH = virtualH * scale;
+
   InitWindow(screenW, screenH, "My First real Raylib Window!");
   SetExitKey(KEY_NULL);
   
 
-  // Load Assets into memory
+  // Load Assets into memory, and sharpen Pixel art
   Image PlayerBox = LoadImage("assets/collisionbox.png");
   ImageResize(&PlayerBox, 32, 64); // scale 2x
   Texture2D player = LoadTextureFromImage(PlayerBox);
@@ -24,12 +32,16 @@ int main(void) {
   // Game Loop
   while (!WindowShouldClose()) {
     // Logic Updates go here ...
+    if (IsKeyDown(KEY_D)) playerPosition.x += playerVelocity;
+    if (IsKeyDown(KEY_A)) playerPosition.x -= playerVelocity;
+    
+
 
     // Drawing the Window and Graphics
     BeginDrawing();
       ClearBackground(RAYWHITE);
       
-      DrawTexture(player, screenW/2 - player.width, screenH/2 - player.height/2, WHITE);
+      DrawTexture(player, playerPosition.x, playerPosition.y, WHITE);
       
 
     EndDrawing();
